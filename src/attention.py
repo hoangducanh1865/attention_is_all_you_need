64655753bed36f8779f11e5d13ccc562633e9d34
Hoàng Đức Anh <160430613+hoangducanh1865@users.noreply.github.com>
2025-12-26 20:54:30 +0700

    Update attention.py

--- src/attention.py
+++ src/attention.py
@@ -22,11 +22,7 @@ class Attention(nn.Module):
             if attention_mask is not None:
                 attention_mask=attention_mask.bool()
                 attention_mask=attention_mask.unsqueeze(1).unsqueeze(1).repeat(1,1,src_len,1)
-            # Use flash attention via torch formula
-            attention_output=F.scaled_dot_product_attention(q,k,v,
-                                                            attn_mask=attention_mask,
-                                                            dropout_p=self.config.attn_drop if self.training else 0.0,
-                                                            is_causal=causal)
+
         else: # Cross attention
             tgt_len=tgt.shape[1]
             q=self.q_proj(tgt).reshape(batch_size,tgt_len,self.config.num_heads,self.head_dim).transpose(1,2)
@@ -35,11 +31,11 @@ class Attention(nn.Module):
             if attention_mask is not None:
                 attention_mask=attention_mask.bool()
                 attention_mask=attention_mask.unsqueeze(1).unsqueeze(1).repeat(1,1,tgt_len,1)
-            # Use flash attention via torch formula
-            attention_output=F.scaled_dot_product_attention(q,k,v,
-                                                            attn_mask=attention_mask,
-                                                            dropout_p=self.config.attn_drop if self.training else 0.0,
-                                                            is_causal=causal)
+        # Use flash attention via torch formula
+        attention_output=F.scaled_dot_product_attention(q,k,v,
+                                                        attn_mask=attention_mask,
+                                                        dropout_p=self.config.attn_drop if self.training else 0.0,
+                                                        is_causal=causal)
         output=attention_output.transpose(1,2).flatten(2) # flatten(2) means to flatten from dimension 2 to make (batch_size, seq_len, num_heads, head_dim) become (batch_size, seq_len, embed_dim)
         output=self.out_proj(output)
         return output
